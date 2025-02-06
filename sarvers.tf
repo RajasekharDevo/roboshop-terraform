4afdcbbeb1696682c4006c86cbb2fbbf74b81e26
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-06 23:15:40 +0530

    terrafrom.server

--- sarvers.tf
+++ sarvers.tf
@@ -4,8 +4,8 @@ data "aws_ami" "centos" {
   name_regex = "Centos-8-DevOps-Practice"
 }
 
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
+data "aws_security_group" "launch-wizard-16" {
+  name = "launch-wizard-16"
 }
 
 variable "instance_type" {
@@ -15,7 +15,7 @@ variable "instance_type" {
 resource "aws_instance" "frontend" {
   ami = data.aws_ami.centos.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+  vpc_security_group_ids = [ data.aws_security_group.launch-wizard-16.id]
   tags = {
     Name = "frontend"
   }
