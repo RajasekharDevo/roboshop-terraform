7b427f017eda31f8f99ffcb567cb7819bf7b5d16
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-07 21:35:48 +0530

    terrafrom.server

--- sarvers.tf
+++ sarvers.tf
@@ -4,31 +4,42 @@ data "aws_ami" "centos" {
   name_regex = "Centos-8-DevOps-Practice"
 }
 
-data "aws_security_group" "launch-wizard-16" {
-  name = "launch-wizard-16"
-}
-
-variable "instance_type" {
-  default ="t2.micro"
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
 }
 
 variable "components" {
-  default = ["frontend", "mongodb", "catalogue"]
-}
-
-resource "aws_instance" "instance" {
-  count        = length(var.components)
-  ami = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.launch-wizard-16.id]
-  tags = {
-    Name = var. components[count.index]
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t2.micro"
+    }
   }
 }
-//resource "aws_route53_record" "frontend" {
- // zone_id = "Z07633651VJKTEQ867N3J"
- // name = "frontend-dev.rdevopsb72.online"
- // type = "A"
- // ttl = 30
-//records = [aws_instance.frontend.private_ip]
-//}
+resource "aws_instance" "instance" {
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+resource "aws_route53_record" "records" {
+  for_each               = var.components
+  zone_id  = "Z07633651VJKTEQ867N3J"
+  name     = "${each.value["name"]}-dev.rdevopsb72.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance[each.value["name"]].private_ip]
+}
