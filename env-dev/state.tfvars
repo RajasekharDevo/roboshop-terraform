562f356a7cbfa76bcdd52e47e0792e185a49646f
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-08 23:06:53 +0530

    terrafrom.server

---
+++ env-dev/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "terrafrom-r72"
+key    = "roboshop/dev/terraform.tfstate"
+region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,8 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "terrafrom-r72"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
 
