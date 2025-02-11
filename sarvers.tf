21fdb20c809ec7d48fd2dd8621446afd872ab538
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-11 23:43:56 +0530

    terrafrom.server

--- sarvers.tf
+++ sarvers.tf
@@ -10,23 +10,27 @@ resource "aws_instance" "instance" {
 }
 
 resource "null_resource" "provisioner" {
+
   depends_on = [aws_instance.instance, aws_route53_record.records]
   for_each = var.components
   provisioner "remote-exec" {
+
     connection {
-      type ="ssh"
-      user = "root"
+      type     = "ssh"
+      user     = "root"
       password = "DevOps321"
-      host =aws_instance.instance[each.value["name"]].private_ip
-}
-inline = [
-  "rm -rf roboshop-shell",
-  "git clone https://github.com/RajasekharDevo/roboshop-shell",
-  "cd roboshop-shell",
-  "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password" , "null") }"
-  ]
+      host     = aws_instance.instance[each.value["name"]].private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/RajasekharDevo/roboshop-shell",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password" , "null") }"
+    ]
   }
 }
+
 
 resource "aws_route53_record" "records" {
   for_each               = var.components
